ad20e7e553623cb50ed6cde6105824eeeb7c4ac1
johnmanjohnston <c.ethanjohn@gmail.com>
2023-06-27 23:09:59 +0400

    End to end encryption works for one user (user's own public key). TODO: implement to work with other user's public keys

--- client.js
+++ client.js
@@ -1,14 +1,78 @@
 const io = require("socket.io-client");
 const socket = io("http://localhost:3000");
 const readline = require("readline");
+const crypto = require("crypto");
+const axios = require("axios");
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-socket.on("msg", (data) => {
-    console.log(data)
+var privateKey = null;
+var publicKey = null;
+var recieverPublicKey = null;
+
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+  
+function generateClientKeys() {
+    return new Promise((resolve, reject) => {
+      crypto.generateKeyPair(
+        "rsa",
+        {
+          modulusLength: 2048,
+          publicKeyEncoding: {
+            type: "spki",
+            format: "pem",
+          },
+          privateKeyEncoding: {
+            type: "pkcs8",
+            format: "pem",
+          },
+        },
+        (err, pubKey, pvtKey) => {
+          if (err) {
+            reject(err);
+          } else {
+            publicKey = pubKey;
+            privateKey = pvtKey;
+            resolve();
+          }
+        }
+      );
+    });
+  }
+
+async function getKey(sender) {
+    var s = null;
+    
+    await axios.get(`http://localhost:3000/getkey/${sender}`).then((data) => {
+        s = data.data.key
+    }).catch( (e) => {} );
+
+    return s;
+}
+
+socket.on("msg", async (data) => {
+    const message = data;
+
+    // console.log(privateKey)
+
+    try {
+        var decrypted = crypto.privateDecrypt(privateKey, message);
+        decrypted = decrypted.toString();
+        console.log(`decrypt: ` + decrypted);
+    } catch (e) {
+        console.log("Recieved a message. Couldn't decrypt.");
+    }
+});
+
+socket.on("public-key-transfer", (data) => {
+    recieverPublicKey = data;
+    console.log("Recieved reciever's public key.")
+    console.log(data);
 });
 
 var ROOM = "";
@@ -20,17 +84,31 @@ function promptUser() {
             process.exit(0);
         }
         
-        readline.moveCursor(process.stdout, 0, -1);
-        readline.clearLine(process.stdout, 0);
+        //readline.moveCursor(process.stdout, 0, -1);
+        //readline.clearLine(process.stdout, 0);
 
-        socket.emit("msg", message);
+        if (!recieverPublicKey) {
+            console.log("Did not recieve reciever's public key");
+            promptUser();
+        }
+
+        var encryptedMessage = null;
+        encryptedMessage = crypto.publicEncrypt(recieverPublicKey, Buffer.from(message));
+
+        // console.log(encryptedMessage);
+
+        socket.emit("msg", encryptedMessage);
         promptUser();
     });
 }
 
 rl.question("Room name: ", (roomname) => {
     ROOM = roomname;
-    socket.emit("join", ROOM);
-
-    promptUser();
+    
+    generateClientKeys();   
+    setTimeout(() => {
+        console.log(`My public key: ${publicKey}`)
+        socket.emit("join", { ROOM, publicKey });
+        promptUser();
+    }, 5000)
 })
--- server.js
+++ server.js
@@ -2,29 +2,46 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http)
+const crypto = require("crypto");
 
 var rooms = {};
+var keys = {};
+
+var keyPairs = {};
+var publicKeys = {};
 
 io.on("connection", (socket) => {
     console.log(`Incoming connection from ${socket.id}`);
     
-    socket.on("join", (roomname) => {
-        socket.join(roomname);
-        rooms[socket.id] = roomname;
-        console.log(`${socket.id} joined ${roomname}`);
+    socket.on("join", ({ ROOM, publicKey }) => {
+        socket.join(ROOM);
+        rooms[socket.id] = ROOM;
+        publicKeys[socket.id] = publicKey;
+        keys[socket.id] = crypto.randomBytes(32).toString("hex");
+        console.log(`${socket.id} joined ${ROOM}`);
 
-        io.to(roomname).emit("msg", `Info: ${socket.id} joined ${roomname}`)
+        io.to(socket.id).emit("public-key-transfer", publicKey)
+        console.log(publicKey)
+        // io.to(ROOM).emit("msg", `Info: ${socket.id} joined ${ROOM}`)
     });
 
     socket.on("msg", (data) => {
-        console.log("Incoming message...");
-        console.log(data)
-
         const roomname = rooms[socket.id];
-        io.to(roomname).emit("msg", `${socket.id}: ${data}`)
+        io.to(roomname).emit("msg", data)
+        console.log("Incoming data: " + data)
     });
 });
 
+app.get("/getkey/:sender", (req, res) => {
+    var k = req.params.sender;
+    k = keys[k];
+
+    if (k) {
+        return res.status(200).send({ key: k });
+    }
+
+    return res.status(404).end();
+});
 
 http.listen(3000, () => {
     console.log("Running on http://localhost:3000");
